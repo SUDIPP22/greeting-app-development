77570a3d0f604edd52953b5a353c271a0aed7e4d
Sudip Panja <panja370@gmail.com>
2021-12-03 04:41:34 +0530

    Added : Dependencies for Greeting App Development

---
+++ src/main/java/com/bridgelabz/GreetingAppDevelopmentApplication.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Purpose : To simulate Greeting Application
+ *
+ * @author SUDIP PANJA
+ * @version : 0.0.1-SNAPSHOT
+ * @since 2021-12-03
+ */
+@SpringBootApplication
+public class GreetingAppDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppDevelopmentApplication.class, args);
+    }
+
+}
